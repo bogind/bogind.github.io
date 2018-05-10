c56694f056c6a5bfd53347791f45acf77619b3a9
Dror Bogin <Dror.Bogin@gmail.com>
2018-05-10 13:25:14 +0300

    Update weather.js

--- docs/Lmap/js/weather.js
+++ docs/Lmap/js/weather.js
@@ -11,7 +11,7 @@ var url = "https://api.openweathermap.org/data/2.5/forecast?lat=31.251155&lon=34
 					var weather = resp;
 					$("#weather-icon").html("<img src='http://openweathermap.org/img/w/"+weather.list[0].weather[0]["icon"]+".png' width='25' height='25'/>");
 					$("#clouds").html(" Cloud Cover: "+ weather.list[0].weather[0].description + ", " + weather.list[0].clouds["all"]+"%");
-					$("#temp").html(" Temperature: "+ weather.list[0].main.temp +
+					$("#temp").html(" Temperature: "+ weather.list[0].main.temp + "&#176" +
 							"<br> Humidity: " + weather.list[0].main.humidity+"%" );
 					if('rain' in weather.list[0] & '3h' in weather.list[0].rain){
 						if(typeof w.responseJSON.list[0].rain["3h"] === "undefined"){
